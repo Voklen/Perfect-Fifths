1e2d1e66b0cd64cd69c2c34ee5409e74f83955e5
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-09-16 08:57:10 +0100

    Start to implement fraction

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,49 @@
-fn approx_equal(a: &f64, b: f64, decimal_places: u8) -> bool {
-	let factor = 10.0f64.powi(decimal_places as i32);
-	let a = (a * factor).trunc();
-	let b = (b * factor).trunc();
-	a == b
+#[derive(PartialEq, PartialOrd)]
+struct Fraction {
+	top: u128,
+	bottom: u128,
+}
+
+impl Fraction {
+	fn powi(self, power: u32) -> Self {
+		let new_top = self.top.pow(power);
+		let new_bottom = self.bottom.pow(power);
+		Fraction { top: new_top, bottom: new_bottom }
+	}
+
+	fn divide(&mut self, number: u128) {
+		let top_div = self.top / number;
+		if self.top == (top_div * number) {
+			self.top = top_div
+		} else {
+			self.bottom *= number
+		}
+	}
+
+	fn minus(&mut self, number: u128) {
+		self.top -= self.bottom * number;
+	}
+}
+
+impl std::fmt::Display for Fraction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		let top = self.top;
+		let bottom = self.bottom;
+        write!(f, "{top}/{bottom}")
+    }
 }
 
 fn main() {
-	const BASE: f64 = 1.5;
+	const BASE: Fraction = Fraction{top: 3, bottom: 2};
 	let mut power = 1;
 	let mut numbers = vec![BASE];
 	loop {
 		power += 1;
 		let mut new = BASE.powi(power);
 
-		while new >= 2.0 {
-			new /= 2.0;
+		let two = Fraction{top: 2, bottom: 1};
+		while new >= two {
+			new.divide(2);
 		}
 		println!("Power {power}: {new}");
 		let result = numbers
